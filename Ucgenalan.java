7dcd8040fa482d3e1a2eacfee4e9cba135c74bbe
İrem Rabia Topal <117042843+iremmr@users.noreply.github.com>
2022-12-12 21:36:49 +0300

    Add files via upload

---
+++ Ucgenalan.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ucgenalan {
+    public static void main(String[] args) {
+        int a,b,c,cevre,u;
+        double alan;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("1. Kenar uzunluğunu giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("2. Kenar uzunluğunu giriniz: ");
+        b = input.nextInt();
+
+        System.out.print("3. Kenar uzunluğunu giriniz: ");
+        c = input.nextInt();
+
+        cevre = a + b + c;
+        u = cevre/2;
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Alan = " + alan);
+
+
+    }
+}
